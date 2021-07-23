d1bac4aea2c5047e50576ffb6f6d37d59f6f2507
ny4rlk0 <55476474+ny4rlk0@users.noreply.github.com>
2021-07-23 19:00:19 +0300

    Update RiotGameManager.py

--- RiotGameManager.py
+++ RiotGameManager.py
@@ -7,29 +7,20 @@ proc_list=["BsSndRpt.exe","LeagueClient.exe","LeagueClientUx.exe","LeagueClientU
 drive_letter_list=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","R","S","T","U","V","Y","Z","X","W"]
 generic_riot_path="\\Riot Games\\Riot Client\\RiotClientServices.exe"
 t='"'
-def list_process(): #List Task runs in Taskmanager
-    process_list = str(sub.check_output(['tasklist']).decode())
-    return process_list
-def chk_process(processname): #Check the process is it running? Why are you running?
-    try:
-        if processname in process_list: return True
-        else: return False
-    except: pass
 def kill_process(): #Kill the game process
-    global process_list
-    process_list = list_process()
     i=0
-    for i in range(len(proc_list)):
-        try: 
-            if chk_process(str(proc_list[i]))==True: os.system(f'taskkill /T /F /IM {t}{proc_list[i]}{t}') #F force T Child Process IM List By Name#kill_process(proc_list[i])
-        except: continue
+    for i in range(2*len(proc_list)):
+        try: sub.Popen(['taskkill','/T','/F','/IM',f'{proc_list[i]}'], stdout=sub.DEVNULL, stderr = sub.DEVNULL , stdin = sub.DEVNULL)
+        except: pass
         i=+1
 def find_game_path(): #Find game path
     i=0
     for i in range(len(drive_letter_list)):
-        gamepath=drive_letter_list[i]+":"+generic_riot_path
-        if os.path.exists(gamepath): return gamepath
-        else: i=+1
+        try:
+            gamepath=drive_letter_list[i]+":"+generic_riot_path
+            if os.path.exists(gamepath): return gamepath
+        except: continue
+        i=+1
 def start_process(game): #Start Game
     gamepath=find_game_path()
     try:
@@ -37,7 +28,7 @@ def start_process(game): #Start Game
         elif game=="valorant": os.system(f"start /b {t}LeagueOfLegends{t} {t}{gamepath}{t} --launch-product=valorant --launch-patchline=live")
     except: pass
 def is_any_button_pressed():
-    time.sleep(0.1) #Lowers the cpu usage!
+    time.sleep(0.1) #Lowers the cpu usage significantly!
     if key.is_pressed("f8"): kill_process()
     elif key.is_pressed("f9"): start_process("lol")
     elif key.is_pressed("f10"): start_process("valorant")
